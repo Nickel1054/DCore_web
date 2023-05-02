985a53c38d46f1a14565b4adab7159f7dab1956b
Nickel1054 <pekarskyi1054@gmail.com>
2023-05-02 13:00:18 +0300

    Added requirement for username.

--- DCore_web/views.py
+++ DCore_web/views.py
@@ -2,10 +2,13 @@
 # from django.http import HttpResponseRedirect
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import UserPassesTestMixin
 from .forms import LoginForm
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
-
+import os
+from dotenv import load_dotenv
+import ast
 
 class HomePage(TemplateView):
     template_name = 'index.html'
@@ -19,10 +22,16 @@ class DocumentationPage(TemplateView):
     template_name = 'documentation.html'
 
 
-class ProfilePage(TemplateView):
+class ProfilePage(UserPassesTestMixin, TemplateView):
     # login_url = 'login/'
     # redirect_field_name = 'email'
     template_name = 'registration/profile.html'
+
+    def test_func(self):
+        load_dotenv()
+        domains = ast.literal_eval(os.getenv('DOMAINS'))
+        return any([self.request.user.username.endswith(i) for i in domains])
+        # return self.request.user.username.endswith('@dtek.com')
 
 # class LogOutPage(TemplateView):
 #     template_name = 'registration/logged_out.html'
